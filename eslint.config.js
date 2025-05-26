d4cb004a597948b4dc8414086f5a42931bf16ed5
LeivaDiego <lei21752@uvg.edu.gt>
2025-05-25 22:47:04 -0600

    Chore: Configure ESLint with StandardJS
    - added no-semicolon rule

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,10 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintConfigStandard from 'eslint-config-standard'
+import eslintPluginImport from 'eslint-plugin-import'
+import eslintPluginN from 'eslint-plugin-n'
+import eslintPluginPromise from 'eslint-plugin-promise'
 
 export default [
   { ignores: ['dist'] },
@@ -13,21 +17,26 @@ export default [
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+        sourceType: 'module'
+      }
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      import: eslintPluginImport,
+      n: eslintPluginN,
+      promise: eslintPluginPromise
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      ...eslintConfigStandard.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },
+        { allowConstantExport: true }
       ],
-    },
-  },
+      semi: ['error', 'never'] // Enforce no semicolons
+    }
+  }
 ]
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from '../assets/react.svg'
 import viteLogo from '../assets/vite.svg'
 import '../styles/App.css'
 
-function App() {
+function App () {
   const [count, setCount] = useState(0)
 
   return (
