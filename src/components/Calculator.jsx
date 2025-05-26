2322ff1ae98709adc9f32b98b579f2243292cc46
LeivaDiego <lei21752@uvg.edu.gt>
2025-05-25 23:05:21 -0600

    Feat: Base structure

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,10 @@
+export default function Calculator () {
+  return (
+    <div className='calculator'>
+      <div className='display'>0</div>
+      <div className='keypad'>
+        {/*TODO: Buttons Here*/}
+      </div>
+    </div>
+  )
+}
